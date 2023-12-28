09932244be4f8e7ffcd60097cdd2f80138de11ca
mtt2701 <48597692+mtt2701@users.noreply.github.com>
2023-12-29 01:39:32 +0800

    minor fix

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Unofficial Home of (the) Harrison Temple
 
-Quotes and images from [Vedal987](https://twitch.tv/vedal987).
+Quotes and images from [Vedal987](https://twitch.tv/vedal987) and [Neuro-sama]("https://www.youtube.com/@_neurosama").
--- app/hello-world/page.js
+++ app/hello-world/page.js
@@ -14,10 +14,10 @@ export default async function Page() {
             </div>
             <div className="mx-auto max-w-prose space-y-8 py-8 ">
                 <article className="prose md:prose-xl prose-primary mx-auto text-center px-8 text-black dark:text-white">
-                    Designed by <Link href="https://github.com/mtt2701/harrison-temple">mtt2701</Link>
+                    Designed by <Link href="https://github.com/mtt2701/harrison-temple" class="text-black dark:text-white">mtt2701</Link>
                 </article>
                 <article className="prose md:prose-xl prose-primary mx-auto text-center px-8 text-black dark:text-white">
-                    Quotes and images from <Link href="https://twitch.tv/vedal987">Vedal987</Link> and <Link href="https://www.youtube.com/@_neurosama">Neuro-sama</Link>
+                    Quotes and images from <Link href="https://twitch.tv/vedal987" class="text-black dark:text-white">Vedal987</Link> and <Link href="https://www.youtube.com/@_neurosama" class="text-black dark:text-white">Neuro-sama</Link>
                 </article>
             </div>
         </Container>
